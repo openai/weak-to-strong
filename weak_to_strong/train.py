9a6ef4e727009701d4c88ff85ea64459fe143138
Ashutosh Srivastava <132872965+srivhash@users.noreply.github.com>
2023-12-22 12:45:05 +0530

    Merged 2 functions

--- weak_to_strong/train.py
+++ weak_to_strong/train.py
@@ -17,12 +17,22 @@ from weak_to_strong.eval import eval_model_acc
 from weak_to_strong.loss import xent_loss
 from weak_to_strong.model import TransformerWithHead
 
-def try_initialize_model_with_kwargs(model_config, custom_kwargs, num_labels=2, linear_probe=False):
+def try_initialize_model_with_kwargs(model_config, custom_kwargs, num_labels=2, linear_probe=False, device_map=None):
     try:
+        # Prepare arguments for model initialization
+        init_args = {
+            "model_config.name": model_config.name,
+            "num_labels": num_labels,
+            "linear_probe": linear_probe,
+            **custom_kwargs
+        }
+
+        # Include device_map if specified
+        if device_map is not None:
+            init_args["device_map"] = device_map
+
         # Attempt to initialize the model with the given arguments
-        return TransformerWithHead.from_pretrained(
-            model_config.name, num_labels=2, linear_probe=linear_probe, **custom_kwargs
-        ).to("cuda")
+        return TransformerWithHead.from_pretrained(**init_args).to("cuda")
     except TypeError as e:
         # Check if the exception is due to an unexpected keyword argument
         if "unexpected keyword argument" in str(e):
@@ -31,33 +41,11 @@ def try_initialize_model_with_kwargs(model_config, custom_kwargs, num_labels=2, 
 
             # Remove the problematic keyword and retry
             new_kwargs = {k: v for k, v in custom_kwargs.items() if k != problem_kwarg}
-            return try_initialize_model_with_kwargs(model_config, new_kwargs, num_labels, linear_probe)
+            return try_initialize_model_with_kwargs(model_config, new_kwargs, num_labels, linear_probe, device_map)
         else:
             # If the error is not due to an unexpected keyword argument, re-raise it
             raise
 
-def try_initialize_model_with_kwargs_2(model_config, custom_kwargs, num_labels=2, linear_probe=False):
-    try:
-        # Attempt to initialize the model with the given arguments
-        return TransformerWithHead.from_pretrained(
-            model_config.name,
-            num_labels=2,
-            device_map="auto",
-            linear_probe=linear_probe,
-            **custom_kwargs,
-        )
-    except TypeError as e:
-        # Check if the exception is due to an unexpected keyword argument
-        if "unexpected keyword argument" in str(e):
-            # Identify the problematic keyword
-            problem_kwarg = str(e).split("'")[1]
-
-            # Remove the problematic keyword and retry
-            new_kwargs = {k: v for k, v in custom_kwargs.items() if k != problem_kwarg}
-            return try_initialize_model_with_kwargs(model_config, new_kwargs, num_labels, linear_probe)
-        else:
-            # If the error is not due to an unexpected keyword argument, re-raise it
-            raise
 
 @dataclass
 class ModelConfig:
@@ -256,8 +244,8 @@ def train_and_save_model(
     # Load the model
     if model_config.model_parallel:
         assert torch.cuda.device_count() > 1, f"you might want more gpus for {model_config.name}"
-        model = try_initialize_model_with_kwargs_2(
-            model_config, custom_kwargs, num_labels=2, linear_probe=linear_probe
+        model = try_initialize_model_with_kwargs(
+            model_config, custom_kwargs, num_labels=2, linear_probe=linear_probe,device_map="auto"
         )
         already_trained = maybe_load_model(model)
         # slight misnomer, more like minibatch_size_per_dp_replica
